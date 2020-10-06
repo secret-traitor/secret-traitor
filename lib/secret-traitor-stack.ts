405fde89ccd5023b80d381ae0ebca0804a903fcf
Douglas Naphas <douglasnaphas@gmail.com>
2020-10-05 21:10:22 -0400

    Create a secrettraitor.com zone, output the NSs
    https://github.com/secret-traitor/secret-traitor/issues/4
    
    This adds a secrettraitor.com hosted zone to the account, and outputs
    the zone's name servers, so that the account that owns the name can
    delegate to the account where this stack is deployed.

--- lib/secret-traitor-stack.ts
+++ lib/secret-traitor-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigw from "@aws-cdk/aws-apigateway";
+import * as route53 from "@aws-cdk/aws-route53";
 import { HitCounter } from "./hitcounter";
 
 export class SecretTraitorStack extends cdk.Stack {
@@ -22,6 +23,28 @@ export class SecretTraitorStack extends cdk.Stack {
     // define an API Gateway REST API resource backed by our "helloWithCounter" function.
     new apigw.LambdaRestApi(this, "Endpoint", {
       handler: helloWithCounter.handler,
+    });
+
+    const hostedZone = new route53.HostedZone(this, "SecretTraitorZone", {
+      zoneName: this.node.tryGetContext("domain"),
+    });
+
+    new cdk.CfnOutput(this, "HostedZoneNameServers", {
+      value:
+        (hostedZone.hostedZoneNameServers &&
+          hostedZone.hostedZoneNameServers.join(",")) ||
+        "",
+    });
+
+    new cdk.CfnOutput(this, "TestOutput1", { value: "example-output" });
+
+    const nameServers =
+      (hostedZone.hostedZoneNameServers &&
+        hostedZone.hostedZoneNameServers.join(",")) ||
+      "";
+
+    new cdk.CfnOutput(this, "Out2", {
+      value: cdk.Fn.join(",", hostedZone.hostedZoneNameServers || [""]),
     });
   }
 }
