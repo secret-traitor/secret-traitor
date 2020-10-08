27d34e4a0c43487429359698c51c416fad8de70f
Douglas Naphas <douglasnaphas@gmail.com>
2020-10-08 18:15:43 -0400

    Use an external Hosted Zone (not from this stack)
    https://github.com/secret-traitor/secret-traitor/issues/4

--- bin/secret-traitor.ts
+++ bin/secret-traitor.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { SecretTraitorStack } from '../lib/secret-traitor-stack';
+import * as cdk from "@aws-cdk/core";
+import { SecretTraitorStack } from "../lib/secret-traitor-stack";
 
 const app = new cdk.App();
-new SecretTraitorStack(app, 'SecretTraitorStack');
+new SecretTraitorStack(app, "SecretTraitorStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
