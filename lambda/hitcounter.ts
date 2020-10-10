e6a9bb25fcd048945a5adbd51c98dfb9d3ba7581
Douglas Naphas <douglasnaphas@gmail.com>
2020-10-09 20:05:03 -0400

    Make a change to prove the CD pipeline works
    https://github.com/secret-traitor/secret-traitor/issues/6

--- lambda/hitcounter.ts
+++ lambda/hitcounter.ts
@@ -89,6 +89,6 @@ exports.handler = async function (event: {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: `Hello, CDK! You have hit ${event.path}\n`,
+    body: `Hello! You have hit the path ${event.path}\n`,
   };
 };
