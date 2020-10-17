6cd5fc8cebbfc49db13bd1dd4cc405ba0ba0f666
Daniel Muller <dmuller@jooraccess.com>
2020-10-16 20:14:04 -0400

    cleanup

--- backend/src/graphql/Notifications/model.ts
+++
@@ -1,4 +0,0 @@
-export interface NotificationPayload {
-    id: number
-    message?: string
-}
--- backend/src/graphql/PlayerGameState/index.ts
+++
@@ -1,6 +0,0 @@
-// import { GamePlayerState } from './typeDefs'
-// export default GamePlayerState
-//
-// export * from './model'
-// export * from './resolvers'
-// export * from './typeDefs'
