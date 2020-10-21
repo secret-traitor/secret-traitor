97003f8d91afb819ae0a202a32ddd3b1825083bc
Douglas Naphas <douglasnaphas@gmail.com>
2020-10-21 08:58:36 -0400

    Make sure failed prod validation fails the build
    https://github.com/secret-traitor/secret-traitor/issues/8

--- itest/verify-hit-counter.sh
+++ itest/verify-hit-counter.sh
@@ -11,7 +11,7 @@ fi
 TEST_PATH=test-path
 URL_WITH_PATH=${URL}/${TEST_PATH}
 RESPONSE=$(curl ${URL_WITH_PATH})
-EXPECTED_RESPONSE="Hello! You requested the path /${TEST_PATH}"
+EXPECTED_RESPONSE="Hello! You requested the paths /${TEST_PATH}"
 if [[ "${RESPONSE}" != "${EXPECTED_RESPONSE}" ]]
 then
   echo "INTEGRATION TEST FAILURE: requested ${URL_WITH_PATH}, expected:"
