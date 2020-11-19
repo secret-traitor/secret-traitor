a5ffa6b16104b11856f0e2f9bc094aa436da0791
Daniel Muller <dmuller@jooraccess.com>
2020-11-19 10:47:32 -0500

    hard code dynamo creds

--- backend/src/clients/dynamo.ts
+++ backend/src/clients/dynamo.ts
@@ -1,12 +1,51 @@
-import AWS from 'aws-sdk'
+import AWS, { DynamoDB, Endpoint } from 'aws-sdk'
+import { GameStatus, GameType } from '@entities/Game'
 
 AWS.config.update({
-    region: 'us-west-2',
-    dynamodb: {
-        endpoint: new AWS.Endpoint(process.env.DYNAMO_HOST ?? 'localhost:8000'),
+    region: 'local',
+    credentials: {
+        accessKeyId: 'qal9pj',
+        secretAccessKey: 'qryy0m',
     },
 })
 
-const dynamodb = new AWS.DynamoDB.DocumentClient()
+export const dynamoDb = new DynamoDB({
+    endpoint: new Endpoint('http://localhost:8000'),
+})
 
-export default dynamodb
+const backoffInterval = 5000 // 5 seconds
+export const waitForTable = (TableName: string) =>
+    dynamoDb
+        .describeTable({ TableName })
+        .promise()
+        .then((data) => {
+            if (data.Table?.TableStatus !== 'ACTIVE') {
+                console.log(
+                    `Table status: ${data.Table?.TableStatus}, retrying in ${backoffInterval}ms...`
+                )
+                return new Promise((resolve) => {
+                    setTimeout(
+                        () => waitForTable(TableName).then(resolve),
+                        backoffInterval
+                    )
+                })
+            } else {
+                return
+            }
+        })
+        .catch((error) => {
+            console.warn(
+                `Table not found! Error below. Retrying in ${backoffInterval} ms...`,
+                error
+            )
+            return new Promise((resolve) => {
+                setTimeout(
+                    () => waitForTable(TableName).then(resolve),
+                    backoffInterval
+                )
+            })
+        })
+
+const dynamoDbDocClient = new DynamoDB.DocumentClient({ service: dynamoDb })
+
+export default dynamoDbDocClient
