386d51dbc7185fff6933e977570b1f894cbc9c72
Douglas Naphas <douglasnaphas@gmail.com>
2020-12-05 20:06:19 -0500

    Import src/server/index, not src/index
    https://github.com/secret-traitor/secret-traitor/issues/14
    
    src/index does things that are not needed in production, like wait for
    DynamoDB to be available and listen on a port.

--- backend/lambda.ts
+++ backend/lambda.ts
@@ -1,7 +1,7 @@
 'use strict'
 
 const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./src/index')
+const app = require('./src/server/index')
 const server = awsServerlessExpress.createServer(app)
 
 exports.handler = (event: any, context: any) =>
