e55680fe7a14e0fddd0fb133edc4dde641c5dd24
Daniel Muller <dmuller@jooraccess.com>
2020-12-31 14:50:02 -0500

    move cdk entrypoint to `lib/index`

---
+++ infrastucture/lib/index.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core'
+import { SecretTraitorStack } from './secret-traitor-stack'
+
+const app = new cdk.App()
+new SecretTraitorStack(app, 'SecretTraitorStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    },
+})
